cedaae4a4b25c15b2ab77b61b2b21b2d5520b217
Arturo Vivar <arthas1237@hotmail.com>
2022-05-07 13:46:41 +0200

    fixed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const io = require("socket.io")(process.env.SOCKET_URI || 8900, {
   cors: {
-    origin: "https://mellifluous-pothos-4e8f59.netlify.app/",
+    origin: "https://mellifluous-pothos-4e8f59.netlify.app",
   },
 });
 
